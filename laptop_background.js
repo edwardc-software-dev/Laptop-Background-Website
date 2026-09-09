5a2cac37df9d9133df5ad7ae19eb741c36c4c3cb
edwardc-software-dev <edward.chae.studios@gmail.com>
2026-09-09 20:46:05 +1000

    Automatic Background Change
    - Changes background video depending on time, still needs to figure out what 3rd background video will play...

---
+++ laptop_background.js
@@ -0,0 +1,45 @@
+const audio = document.getElementById("aud");
+const wallpaper = document.getElementById("background");
+
+function setRandomPlay() {
+    if (audio.readyState >= 1) {
+        audio.currentTime = Math.random() * audio.duration;
+        audio.play().catch(error => {
+            console.log("Playback blocked, waiting for user input: ", error);
+        });
+        console.log("Audio playing: ", audio.paused);
+    }
+    else {
+        audio.addEventListener("loadedmetadata", () => {
+            audio.currentTime = Math.random() * audio.duration;
+            audio.play().catch(error => {
+                console.log("Playback blocked: waiting for user input", error)
+            });
+            console.log("Audio duration: ", audio.duration);
+        }, { once: true });
+    }
+}
+function playBackground(url) {
+    if (wallpaper.getAttribute("src") != url) {
+        wallpaper.setAttribute("src", url);
+        wallpaper.load();
+    }
+}
+function changeBackground() {
+    const hours = new Date().getHours();
+    if (hours >= 18 || hours < 1) {
+        playBackground("https://archive.org/download/main_loop/main_loop.mp4");
+    }
+    else if (hours >= 1 || hours < 8) {
+        playBackground("https://archive.org/download/nightcord/nightcord.mp4");
+    }
+    else {
+        playBackground("https://archive.org/download/main_loop/main_loop.mp4");
+    }
+    setTimeout(changeBackground, 15 * 60 * 1000);
+}
+document.addEventListener("DOMContentLoaded", setRandomPlay);
+document.addEventListener("DOMContentLoaded", changeBackground);
+document.addEventListener("click", () => audio.paused && setRandomPlay(), { once: true });
+
+
